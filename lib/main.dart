2a60212f5fbcaeffb620cea3be34b1083449cfcb
RENATO AUGUSTO PEREIRA <87014952+RenatoPereira1@users.noreply.github.com>
2022-10-27 11:37:37 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,21 @@ import 'pagina.dart';
 void main() {
   runApp(const MyApp());
 }
-
+// criação da classe MyApp que é obrigatoria e utilização do estado StatelessWidget para informações estáticas
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // utilizamos o @override para sobrescrever um método de uma classe mãe
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Testes Renato',//titulo da pagina
       theme: ThemeData(
-        // criei as cores do tema
+        // criar o tema e suas cores
         primarySwatch: Colors.red,
         brightness: Brightness.dark,
       ),
-      home: const PaginaInicial(),  //chamando a pagina inicial
+      home: const PaginaInicial(),  //chamando a pagina inicial no outro arquivo
     );
   }
 }
